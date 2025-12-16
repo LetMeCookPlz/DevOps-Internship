618141b889969283fb1fb86e6f66b301b94c7fff
Anon <169835788+LetMeCookPlz@users.noreply.github.com>
2025-12-16 21:15:38 +0200

    enable remote Terraform state

--- Individual-3-AWS/providers.tf
+++ Individual-3-AWS/providers.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "tymur-s3-tf-state"
+    key            = "terraform/terraform.tfstate"
+    region         = "eu-central-1" 
+  }
 }
 
 provider "aws" {
